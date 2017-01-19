9342762aa2b1c6eeca8033e12c8af8a72afc9aac
Robson Francisco de Souza <robfsouza@gmail.com>
2017-01-19 00:44:37 -0200

    Edirect is back...

---
+++ edirect.rb
@@ -0,0 +1,24 @@
+class Edirect < Formula
+  desc "Access NCBI's databases from the shell"
+  homepage "http://www.ncbi.nlm.nih.gov/books/NBK179288/"
+  head "ftp://ftp.ncbi.nlm.nih.gov/entrez/entrezdirect/versions/current/edirect.tar.gz"
+  # tag "bioinformatics"
+
+  depends_on "HTML::Entities" => :perl
+  depends_on "LWP::Simple" => :perl
+  depends_on "LWP::Protocol::https" => :perl
+  depends_on "go" => :build
+  depends_on "godep" => :build
+
+  def install
+    doc.install "README"
+    libexec.install "setup.sh", "setup-deps.pl"
+    system "go", "build", "xtract.go"
+    rm ["Mozilla-CA.tar.gz", "xtract.go"]
+    bin.install Dir["*"]
+  end
+
+  test do
+    system bin/"esearch", "-version"
+  end
+end
